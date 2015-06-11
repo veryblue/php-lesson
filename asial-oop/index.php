e6a7f34eba91a07699d81ef1a70458bbc1201288
veryblue <veryblue0416@gmail.com>
2015-06-11 14:09:27 +0900

    Add オブジェクトの破棄

--- asial-oop/index.php
+++ asial-oop/index.php
@@ -8,6 +8,11 @@ $hanako = new Person('アシアル花子', 18);
 //$taro->setAge(20);
 
 $taro->sayHello();
+
+unset($taro); // オブジェクトの破棄
+// $tato = "hogehoge"; // こっちでも破棄される
+
+
 $hanako->sayHello();
 
 //echo 'test';
