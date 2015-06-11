c350d5f93739e5ac8e9a2dd769e388a42cf47b9f
veryblue <veryblue0416@gmail.com>
2015-06-11 17:09:22 +0900

    Add try catch 追加

--- asial-oop/Exception.php
+++ asial-oop/Exception.php
@@ -3,6 +3,13 @@
 // エラー出力する場合
 ini_set( 'display_errors', 1 );
 
-throw new Exception('はじめてのException');
+try {
+	echo 'try開始';
+	throw new Exception('はじめてのException');
+	echo 'try終了';
+} catch (Exception $e) {
+	echo $e->getMessage();
+}
+
 
 
