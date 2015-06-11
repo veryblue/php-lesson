32c4d057ec83ac06f07ba0e824249fb9c1492f12
veryblue <veryblue0416@gmail.com>
2015-06-11 14:01:03 +0900

    Add コンストラクタを利用

--- asial-oop/Person.php
+++ asial-oop/Person.php
@@ -4,6 +4,16 @@ class Person
 {
     private $name = '';
 
+    public function __construct($name)
+    {
+    	$this->setName($name);
+    }
+
+    public function __destruct()
+    {
+    	echo 'さようなら';
+    }
+
     public function setName($name)
     {
     	$this->name = trim($name);
--- asial-oop/index.php
+++ asial-oop/index.php
@@ -2,9 +2,10 @@
 
 require_once('Person.php');
 
-$taro = new Person();
-$taro->setName('アシアル太郎');
+$taro = new Person('アシアル太郎');
+$hanako = new Person('アシアル花子');
 
 $taro->sayHello();
+$hanako->sayHello();
 
 //echo 'test';
