aa781547c865b8b62b9ee9d68123a29c9e69714c
veryblue <veryblue0416@gmail.com>
2015-07-06 23:37:46 +0900

    Add DateTime

---
+++ time.php
@@ -0,0 +1,14 @@
+<?php
+
+$now = new DateTime('', new DateTimeZone('Asia/Tokyo'));
+echo $now->format('Y-m-d H:i:s');
+
+echo '<br>';
+
+date_default_timezone_set('Asia/Tokyo');
+$now = date('Y-m-d H:i:s');
+echo $now;
+
+
+
+
